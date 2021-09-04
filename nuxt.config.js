feeda653bc795e48d824596d71c4ed880b9926a0
Erik Nygren <erik.nygren6@gmail.com>
2021-09-04 21:17:26 +0100

    lol, can't have it working on both gh pages and the domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,6 @@ import getRoutes from './utils/getRoutes'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/personal-site/',
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Erik Nygren: software developer - portfolio site',
