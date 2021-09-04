4ec77a29d11f45b5382009e3795ab44d92efebf1
Erik Nygren <erik.nygren6@gmail.com>
2021-09-04 20:38:39 +0100

    Don't purge used css from MD content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   plugins: [require('@tailwindcss/typography')],
+  purge: {
+    content: [
+      `components/**/*.{vue,js}`,
+      `layouts/**/*.vue`,
+      `pages/**/*.vue`,
+      `plugins/**/*.{js,ts}`,
+      `nuxt.config.{js,ts}`,
+      'content/**/**.md',
+    ],
+  },
 }
