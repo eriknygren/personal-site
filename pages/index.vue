f8d6d2386ab7939aeace18268c8dc01d705aa695
Erik Nygren <erik.nygren6@gmail.com>
2021-09-04 19:59:09 +0100

    Get working without a logo for now

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="container min-h-screen flex flex-col">
     <div class="flex-grow grid grid-cols-1 md:grid-cols-3 md:pt-4">
-      <div class="md:col-span-2 w-full">
-        <img
-          class="splash-img mx-auto"
-          src="https://picsum.photos/800/600?grayscale"
-        />
-      </div>
-      <div class="md:col-span-1 w-full text-left flex flex-col px-2">
+      <div class="md:col-span-2 w-full text-left">
         <div class="flex-grow">
-          <h1
-            class="text-3xl lg:text-5xl font-semibold select-none pt-4 md:pt-0"
-          >
+          <h1 class="text-3xl lg:text-5xl font-semibold select-none pt-4">
             Hej, I'm Erik
           </h1>
           <p class="text-2xl pt-4">
@@ -22,8 +14,11 @@
             <span class="color-loop-4">real time performance</span>.
           </p>
         </div>
-        <div class="md:text-right pt-4 pb-4 md:pt-0 md:pb-0">
-          <div class="text-right pb-1">
+      </div>
+      <div class="md:col-span-1"></div>
+      <div class="md:col-span-3 w-full flex flex-col px-2 justify-end">
+        <div class="text-left md:text-right pt-4 pb-4 md:pt-0 md:pb-0">
+          <div class="text-right pb-2">
             <a
               href="https://github.com/eriknygren"
               target="_blank"
@@ -44,7 +39,7 @@
     <div class="w-full flex flex-col flex-grow">
       <div>
         <h3
-          class="text-2xl select-none pb-4 pt-4 border-t border-black md:border-0"
+          class="text-2xl select-none pb-4 pt-4 md:pt-0 border-t border-black md:border-0"
         >
           SELECTED WORK
         </h3>
