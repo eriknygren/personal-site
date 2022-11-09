f71fc907be49cd08d0d6ab7dfd9a007aae8720f4
Erik Nygren <erik.nygren6@gmail.com>
2022-11-09 12:55:47 +1100

    get tailwind working (somewhat)

--- nuxt3/nuxt-app/pages/index.vue
+++ nuxt3/nuxt-app/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
+  <div class="container min-h-screen flex flex-col">
     <div>gday</div>
-    <div v-if="portfolio">
-      <div v-for="(entry, index) in portfolio" :key="`entry-${index}`">
-        <portfolio-card :article="entry" />
+    <div class="w-full flex flex-col flex-grow">
+      <div>
+        <h3
+          class="text-2xl select-none pb-4 pt-4 md:pt-0 border-t border-black md:border-0"
+        >
+          SELECTED WORK
+        </h3>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-3 w-full">
+        <div v-for="(entry, index) in portfolio" :key="`entry-${index}`">
+          <portfolio-card class="flex-grow" :article="entry" />
+        </div>
       </div>
     </div>
   </div>
