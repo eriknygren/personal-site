d5225b95e8b6268c015ec731c0d0088d33276c8c
Erik Nygren <erik.nygren6@gmail.com>
2023-02-28 11:39:40 +0000

    Get GA back in there

---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,11 @@
+import VueGtag, { trackRouter } from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    enabled: process.env.NODE_ENV === 'production',
+    property: {
+      id: 'G-363DMVGGTX',
+    }
+  })
+  trackRouter(useRouter())
+})
